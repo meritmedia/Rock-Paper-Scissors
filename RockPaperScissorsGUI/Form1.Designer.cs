715db1192d9157cbba59b8818bb941fa50614de0
merit media <github@meritmedia.com>
2017-05-26 08:57:17 -0700

    Add project files.

---
+++ RockPaperScissorsGUI/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace RockPaperScissorsGUI
+{
+    partial class frmRPS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnComputer = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.lblComputerWinner = new System.Windows.Forms.Label();
+            this.lblYouWins = new System.Windows.Forms.Label();
+            this.lblCompWins = new System.Windows.Forms.Label();
+            this.lblYouPlayer = new System.Windows.Forms.Label();
+            this.lblPlayerComputer = new System.Windows.Forms.Label();
+            this.btnDonate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(12, 441);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(254, 54);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnComputer
+            // 
+            this.btnComputer.Image = global::RockPaperScissorsGUI.Properties.Resources.computer;
+            this.btnComputer.Location = new System.Drawing.Point(316, 234);
+            this.btnComputer.Name = "btnComputer";
+            this.btnComputer.Size = new System.Drawing.Size(156, 64);
+            this.btnComputer.TabIndex = 3;
+            this.btnComputer.UseVisualStyleBackColor = true;
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Image = global::RockPaperScissorsGUI.Properties.Resources.scissors_l;
+            this.btnScissors.Location = new System.Drawing.Point(46, 304);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(156, 64);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Image = global::RockPaperScissorsGUI.Properties.Resources.paper_l;
+            this.btnPaper.Location = new System.Drawing.Point(46, 230);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(156, 68);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnRock
+            // 
+            this.btnRock.Image = global::RockPaperScissorsGUI.Properties.Resources.rock_l;
+            this.btnRock.Location = new System.Drawing.Point(46, 156);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(156, 68);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.BackColor = System.Drawing.Color.Transparent;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(53, 381);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(0, 25);
+            this.lblWinner.TabIndex = 5;
+            // 
+            // lblComputerWinner
+            // 
+            this.lblComputerWinner.AutoSize = true;
+            this.lblComputerWinner.BackColor = System.Drawing.Color.Transparent;
+            this.lblComputerWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerWinner.Location = new System.Drawing.Point(318, 381);
+            this.lblComputerWinner.Name = "lblComputerWinner";
+            this.lblComputerWinner.Size = new System.Drawing.Size(0, 25);
+            this.lblComputerWinner.TabIndex = 6;
+            // 
+            // lblYouWins
+            // 
+            this.lblYouWins.AutoSize = true;
+            this.lblYouWins.BackColor = System.Drawing.Color.Transparent;
+            this.lblYouWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYouWins.Location = new System.Drawing.Point(53, 413);
+            this.lblYouWins.Name = "lblYouWins";
+            this.lblYouWins.Size = new System.Drawing.Size(91, 25);
+            this.lblYouWins.TabIndex = 7;
+            this.lblYouWins.Text = "Wins: 0";
+            // 
+            // lblCompWins
+            // 
+            this.lblCompWins.AutoSize = true;
+            this.lblCompWins.BackColor = System.Drawing.Color.Transparent;
+            this.lblCompWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompWins.Location = new System.Drawing.Point(320, 413);
+            this.lblCompWins.Name = "lblCompWins";
+            this.lblCompWins.Size = new System.Drawing.Size(91, 25);
+            this.lblCompWins.TabIndex = 8;
+            this.lblCompWins.Text = "Wins: 0";
+            // 
+            // lblYouPlayer
+            // 
+            this.lblYouPlayer.AutoSize = true;
+            this.lblYouPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.lblYouPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYouPlayer.Location = new System.Drawing.Point(90, 107);
+            this.lblYouPlayer.Name = "lblYouPlayer";
+            this.lblYouPlayer.Size = new System.Drawing.Size(78, 37);
+            this.lblYouPlayer.TabIndex = 9;
+            this.lblYouPlayer.Text = "You";
+            // 
+            // lblPlayerComputer
+            // 
+            this.lblPlayerComputer.AutoSize = true;
+            this.lblPlayerComputer.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayerComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerComputer.Location = new System.Drawing.Point(310, 107);
+            this.lblPlayerComputer.Name = "lblPlayerComputer";
+            this.lblPlayerComputer.Size = new System.Drawing.Size(166, 37);
+            this.lblPlayerComputer.TabIndex = 10;
+            this.lblPlayerComputer.Text = "Computer";
+            // 
+            // btnDonate
+            // 
+            this.btnDonate.Location = new System.Drawing.Point(272, 441);
+            this.btnDonate.Name = "btnDonate";
+            this.btnDonate.Size = new System.Drawing.Size(242, 54);
+            this.btnDonate.TabIndex = 11;
+            this.btnDonate.Text = "Donate $0.99 to the Be Your Own Hero Foundation";
+            this.btnDonate.UseVisualStyleBackColor = true;
+            this.btnDonate.Click += new System.EventHandler(this.btnDonate_Click);
+            // 
+            // frmRPS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RockPaperScissorsGUI.Properties.Resources.bkg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(526, 519);
+            this.Controls.Add(this.btnDonate);
+            this.Controls.Add(this.lblPlayerComputer);
+            this.Controls.Add(this.lblYouPlayer);
+            this.Controls.Add(this.lblCompWins);
+            this.Controls.Add(this.lblYouWins);
+            this.Controls.Add(this.lblComputerWinner);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnComputer);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Name = "frmRPS";
+            this.Text = "Rock, Paper, Scissors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Button btnComputer;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Label lblComputerWinner;
+        private System.Windows.Forms.Label lblYouWins;
+        private System.Windows.Forms.Label lblCompWins;
+        private System.Windows.Forms.Label lblYouPlayer;
+        private System.Windows.Forms.Label lblPlayerComputer;
+        private System.Windows.Forms.Button btnDonate;
+    }
+}
+
---
+++ RockPaperScissorsGUI/Form1.cs
@@ -0,0 +1,140 @@
+﻿/*
+ * Name: Charles Pecot
+ * Date: Sept 29, 2015
+ * Project: Exercise 4.8, RockPaperScissorsGUI
+ * Demonstration of If ELSE and SWITCH logic.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace RockPaperScissorsGUI
+{
+    public partial class frmRPS : Form
+    {
+        int  youWins = 0, compWins = 0;
+        enum Hands
+        {
+            ROCK = 1, PAPER, SCISSORS
+        }
+        public frmRPS()
+        {
+            InitializeComponent();
+        }
+        // das buttons
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            whoWon((int)Hands.ROCK, ComputerPick());
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            whoWon((int)Hands.PAPER, ComputerPick());
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            whoWon((int)Hands.SCISSORS, ComputerPick());
+        }
+
+        // called from whoWon
+        private void YouPick(int you)
+        {
+            //set the user's choices to their dimmed versions
+            btnPaper.Image = ((System.Drawing.Image)(Properties.Resources.paper_l_no));
+            btnRock.Image = ((System.Drawing.Image)(Properties.Resources.rock_l_no));
+            btnScissors.Image = ((System.Drawing.Image)(Properties.Resources.scissors_l_no));
+            switch (you)
+            {  // reset the user's pick to normal
+                case 1: //ROCK
+                    btnRock.Image = ((System.Drawing.Image)(Properties.Resources.rock_l));
+                    break;
+                case 2: //PAPER
+                    btnPaper.Image = ((System.Drawing.Image)(Properties.Resources.paper_l));
+                    break;
+                case 3: //SCISSORS
+                    btnScissors.Image = ((System.Drawing.Image)(Properties.Resources.scissors_l));
+                    break;
+                default: //SOMETHING ELSE
+                    btnScissors.Image = ((System.Drawing.Image)(Properties.Resources.computer));
+                    break;
+            }
+            
+        }
+        // The computer's pick
+        private int ComputerPick()
+        {
+            Random rnd = new Random();
+            int choice = rnd.Next(1, Enum.GetNames(typeof(Hands)).Length+1);
+            // change the computer's picked image
+            switch (choice)
+            {
+                case 1:
+                    btnComputer.Image = ((System.Drawing.Image)(Properties.Resources.rock_r));
+                    break;
+                case 2:
+                    btnComputer.Image = ((System.Drawing.Image)(Properties.Resources.paper_r));
+                    break;
+                case 3:
+                    btnComputer.Image = ((System.Drawing.Image)(Properties.Resources.scissors_r));
+                    break;
+                default:
+                    btnComputer.Image = ((System.Drawing.Image)(Properties.Resources.computer));
+                    break;
+            }
+            return choice;  // return the value to be used in method "whoWon".
+        }
+        private void whoWon(int youVal, int computerVal)
+        {
+            YouPick(youVal); // display what the user picked
+            // commpare the user's choice with the computer's choice.
+            // Could also be coded as
+            // (youVal==1 && computerVal==3) || (youVal==2 && computerVal==1) || (youVal==3 && computerVal==2)
+            // But it may just be easier to say "rock (1) beats scissors (3), difference is -2,
+            //                                   paper (2) beats rock (1), difference is 1,
+            //                               and scissors (3) beats paper (2), difference is 1".
+            // Now you're making two comparisons rather than 3
+            // BTW, the differences the other way round are -1 and 2 (the same numbers * -1)
+            int diff = youVal - computerVal;
+            string winningHand = "";
+            if (diff == 0)
+            {
+                lblWinner.Text =  "Tie!";
+                lblComputerWinner.Text =  "Tie!";
+            }
+            else if (diff == 1 || diff == -2)
+            {
+                winningHand = Enum.GetName(typeof(Hands), youVal);
+                lblWinner.Text =  winningHand + " wins!";
+                lblComputerWinner.Text = "";
+                lblYouWins.Text = "Wins: " + ++youWins;
+            }
+            else
+            {
+                winningHand = Enum.GetName(typeof(Hands), computerVal);
+                lblComputerWinner.Text = winningHand + " wins!";
+                lblWinner.Text = "";
+                lblCompWins.Text = "Wins: " + ++compWins; 
+            }
+             
+        }
+
+        private void btnDonate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Opening up your default browser...");
+            Process.Start("http://beyourownhero.us/donations.html#rockpaperscissors");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
